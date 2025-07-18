bfce4a2b868138307b546ea3066d63bf868f0104
haxxorlarn16 <lauren.jonesw@gmail.com>
2025-07-17 23:35:44 -0400

    Initial Commit

---
+++ lazygirltravelhacksTests/lazygirltravelhacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  lazygirltravelhacksTests.swift
+//  lazygirltravelhacksTests
+//
+//  Created by Lauren Jones on 7/17/25.
+//
+
+import Testing
+@testable import lazygirltravelhacks
+
+struct lazygirltravelhacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
